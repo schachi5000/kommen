beac96c94439d0e48620f7cc939bb66553193f38
alexander <alex.schacher@gmail.com>
2024-03-04 16:11:18 +0100

    :green_heart: bumped open JDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.github.schachi5000:kommon-android:dev-SNAPSHOT")
 }
